3c26708639d4cb6a81b617e132ccc3212798959b
Kévin Casagrande <github.kcasagrande@xoxy.net>
2024-03-13 14:06:18 +0100

    Use getopt for members listing

--- membersOfYear/membersOfYear.sh
+++ membersOfYear/membersOfYear.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env sh
 
+options=$(getopt --options y: --longoptions year: -- "$@")
+eval set -- "$options"
+
+while true; do
+	case "${1}" in
+		-y|--year)
+			argYear="${2}"
+			shift 2
+		;;
+		--)
+			shift
+			break
+		;;
+		*)
+			break
+		;;
+	esac
+done
+
 DATABASE="${1}"
-YEAR="${2}"
+year="'${argYear:?}'"
 
 sqlite3 -readonly -table "${DATABASE}" <<- END
-	.parameter set :year ${YEAR}
+	.parameter set :year ${year}
 
 	.read "$(dirname "${0}")/membersOfYear.sql"
 END
-
